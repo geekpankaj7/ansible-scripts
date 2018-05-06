0b0f510cfaa5816fc162521216201b1acd3be54f
pankaj <pankaj@pankaj21789.debian.local>
2018-05-06 21:56:20 +0530

    update1

--- script_ansible_host.sh
+++ script_ansible_host.sh
@@ -1,1 +1,5 @@
-count=1;echo '[jenkins]';for i in `vagrant ssh-config|grep -E '\w+\.\w+\.\w+\.\w+'|awk '{ print $2}'`;do echo jenkins$count ansible_host=$i ansible_user=vagrant;count=$(( $count + 1 ));done |sudo tee -a /etc/ansible/hosts
+echo -e "[master]
+debian-master ansible_host=192.168.121.1 ansible_user=pankaj
+
+[jenkins]"|sudo tee /etc/ansible/hosts
+count=1;for i in `vagrant ssh-config|grep -E '\w+\.\w+\.\w+\.\w+'|awk '{ print $2}'`;do echo jenkins$count ansible_host=$i ansible_user=vagrant;count=$(( $count + 1 ));done |sudo tee -a /etc/ansible/hosts
